368015cd5cd4d782a71e51799aada5c360b4ccd2
Martin Adamko <martinadamko.sk@gmail.com>
2020-11-09 11:05:57 +0100

    Reorder requires

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # php
 Extended PHP
+
--- src/Functions/index.php
+++ src/Functions/index.php
@@ -1,7 +1,7 @@
 <?php
 // This list of required files was automatically generated:
 
-require_once(__DIR__.'/array/index.php');
 require_once(__DIR__.'/escapeshellpath.php');
+require_once(__DIR__.'/array/index.php');
 
 //------------------ DO NOT EDIT ABOVE THIS LINE ------------------//
