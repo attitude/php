fbc004d3304e2939bc3c8c07ef8e7353c74e873f
Martin Adamko <martinadamko.sk@gmail.com>
2020-11-09 11:03:06 +0100

    Update pre-commit

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/php
-<?php echo shell_exec("./vendor/attitude/index-php/run.sh -path src -backup false");
+<?php echo shell_exec("./vendor/attitude/index-php/run.sh -path src -backup false && git add **/\*.php");
