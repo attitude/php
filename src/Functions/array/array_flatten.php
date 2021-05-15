5d8c116575946cfe4587440a462e30f0669370e6
Martin Adamko <attitude@users.noreply.github.com>
2021-05-15 09:35:21 +0200

    Update array_flatten.php

--- src/Functions/array/array_flatten.php
+++ src/Functions/array/array_flatten.php
@@ -4,7 +4,13 @@ if (!function_exists('array_flatten')) {
   function array_flatten(array $array) {
     $return = array();
 
-    array_walk_recursive($array, function($a) use (&$return) { $return[] = $a; });
+    array_walk_recursive($array, function($item, $key) use (&$return) {
+      if (is_int($key)) {
+        $return[] = $item;
+      } else {
+        $return[$key] = $item;
+      }
+    });
 
     return $return;
   }
